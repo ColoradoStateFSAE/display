c75be62360e0e529c9cf417223b13a92c2ea39a5
Aidan Farley <45497901+Sandman224@users.noreply.github.com>
2025-02-17 18:44:15 -0700

    Update build.sh
    Fixed logic error

--- build.sh
+++ build.sh
@@ -15,16 +15,16 @@ echo "UPDATING DBC FILES"
 # Get all the URLs of the release files
 URLS=$(wget -qO- https://api.github.com/repos/ColoradoStateFSAE/dbc/releases/latest | grep "browser_download_url" | cut -d '"' -f 4)
 
-# Loop over each URL and download the corresponding file
-for URL in $URLS; do
-    echo "Downloading: $URL"
-    wget --show-progress -q "$URL" -O "$DBC_DIR/$(basename "$URL")"
-done
-
 # Remove existing files that have the same name as the new downloads (if needed)
 for URL in $URLS; do
     FILE_NAME=$(basename "$URL")
     rm -f "$DBC_DIR/$FILE_NAME"
+done
+
+# Loop over each URL and download the corresponding file
+for URL in $URLS; do
+    echo "Downloading: $URL"
+    wget --show-progress -q "$URL" -O "$DBC_DIR/$(basename "$URL")"
 done
 
 echo "DBC DOWNLOAD COMPLETE"
